a609baa8e65e950335771b58c23b1b2ee4feef76
Md. Ratan Chowdhury <ratanchowdhury360>
2025-11-19 21:38:41 +0600

    Add mongodb

--- index.js
+++ index.js
@@ -7,6 +7,44 @@ const port = process.env.PORT  || 3000 ;
 app.use(cors());
 app.use(express.json());
 
+
+//charity-donation-platform
+//XHfrvaG4K7vT4TKg
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://charity-donation-platform:<db_password>@cluster0.u0uz2db.mongodb.net/?appName=Cluster0";
+
+const uri = "mongodb+srv://charity-donation-platform:XHfrvaG4K7vT4TKg@cluster0.u0uz2db.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 app.get('/',(req,res)=>{
     res.send('Coffee server is getting hotter')
 })
@@ -15,3 +53,4 @@ app.listen(port, ()=>{
     console.log(`Coffee server running on port ${port}`)
 })
  
+
